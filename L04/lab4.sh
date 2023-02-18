db400342e3351a6ae324a15be0498b1fdd995555
scythemenace <pankur216@gmail.com>
2023-02-17 23:53:43 -0500

    Making changes to Bash code and pushing L04 again

--- L04/lab4.sh
+++ L04/lab4.sh
@@ -1,63 +1,74 @@
 #!/bin/bash
-
 # Use First Argument As Working Directory
 WDIR="$(pwd)"
 cd "$WDIR"
 
 if [ $# -eq 3 ] ; then
-    if [ ! -f "$3".txt ] ; then
-        touch "$3".txt
-        echo "$USER" > "$3".txt
 
-    elif [ ! -d backup ] ; then
-        mkdir backup
-        cp "$3".txt backup
-    fi
+        if [ "$2" -eq 1 ] ; then
+            if [ ! -e "$3" ] ; then
+                touch "$3"
+                echo "$USER" > "$3"
+            fi
+
+        elif [ "$2" -eq 2 ] ; then
+            if [ -e "$3" ] ; then
+                mkdir backup
+                cp "$3" backup
+            fi
 
 
-    if [ $2 -eq 3 ] ; then
-        if [ -f backup/"$3".txt ] ; then
-            cp backup/"$3".txt "$WDIR"
-            rm backup/"$3".txt
+        elif [ "$2" -eq 3 ] ; then
+            if [ -e backup/"$3" ] ; then
+                cp backup/"$3" "$WDIR"
+                rm backup/"$3"
+
+            fi
+
+      fi
+
+
+
+elif [ $# -lt 3 ] ; then
+
+    if [ "$2" -eq 1 ] ; then
+        if [ ! -e file1.txt ] ; then
+            touch file1.txt
+              echo "$USER" > file1.txt
         fi
-    fi
+
+    elif [ "$2" -eq 2 ] ; then
+        if [ -e file1.txt ] ; then
+            mkdir backup
+            cp file1.txt backup
+        fi
 
 
-
-
-
-else
-    if [ ! -f file1.txt ] ; then
-        touch file1.txt
-        echo "$USER" > file1.txt
-
-    elif [ ! -d backup ] ; then
-        mkdir backup
-        cp file1.txt backup
-    fi
-
-
-    if [ $2 -eq 3 ] ; then
-	if [ -f backup/file1.txt ] ; then
+    elif [ "$2" -eq 3 ] ; then
+        if [ -e backup/file1.txt ] ; then
             cp backup/file1.txt "$WDIR"
             rm backup/file1.txt
-	fi
+
+        fi
+
     fi
 
-fi
 
 
 
-counter=1
-if [ $# -gt 2 ] ; then
+elif [ $# -gt 3 ] ; then
+    count=1
     touch excess.txt
-    for var in "$@"
-    do
-	if [ $counter -gt 3 ] ; then
-            echo "$var" >> excess.txt
-	fi
-	counter=$((counter+1))
-    done
+    for arg in "$@"
+        do
+                if [ $count -gt 3 ] ; then
+                echo "$arg" >> excess.txt
+                fi
+                count=$((count+1))
+      done
+
+
+
 fi
 
 echo "Finished"
